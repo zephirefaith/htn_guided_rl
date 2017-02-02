11ad5915d4798804211ecf0a2c42e5e7a9c53a0d
Priyam <priyam8parashar@gmail.com>
2017-02-01 22:00:59 -0800

    added HTN specific parameters;new draw();new obs

--- mdp_rl.py
+++ mdp_rl.py
@@ -38,7 +38,6 @@ class TabQAgent:
     """Tabular one-step Q-learning agent for discrete state/action spaces."""
 
     def __init__(self):
-        self.epsilon = 0.01 # chance of taking a random action instead of the best
         # initializing logging services
         self.logger = logging.getLogger(__name__)
         if False: # True if you want to see more information
@@ -52,12 +51,21 @@ class TabQAgent:
         self.decompose_action = {"slot 0":["hotbar.0 1", "hotbar.0 0"], "slot 1":["hotbar.1 1", "hotbar.1 0"]}
         # q-learning specific
         self.q_table = {}
-        self.gamma = 0.75
+        self.gamma = 0.70
         self.learning_rate = 0.85
+        self.exploration="e-greedy"
         # gold_room specific
         self.min_x = -70
         self.min_y = 13
         self.min_z = -54
+        # HTN specific
+        self.relevant_items = ['gold']
+        room = ['wall', 'stairs']
+        scenario = 0 # change for different scenarios
+        if room[scenario] = 'wall':
+            self.relevant_items.append('glass')
+        else
+            self.relevant_items.append('brick')
 
     def updateQTable( self, reward, current_state ):
         """Change q_table to reflect what we have learnt."""
@@ -112,15 +120,27 @@ class TabQAgent:
 
     def act(self, world_state, agent_host, current_r ):
         """take 1 action in response to the current world state"""
-        # TODO make new state depending upon the gained knowledge from HTN
+        # make new knowledge based state for MDP
         obs_text = world_state.observations[-1].text
         obs = json.loads(obs_text) # most recent observation
+        # debug log information and store as current_s
         self.logger.debug(obs)
+        try:
+            los = ob[u'LineOfSight']
+        except RuntimeError as e:
+            print
+            print 'ERROR: ',e
+            exit(1)
+        block_type = los[u'type']
+        in_range = los[u'inRange']
+        current_s = [block_type, in_range, self.object_in_hand, self.pitch_count, self.prev_a]
+        if block_type in self.relevant_items:
+            current_r += 50
         if not u'XPos' in obs or not u'ZPos' in obs:
             self.logger.error("Incomplete observation received: %s" % obs_text)
             return 0
-        current_s = "%d:%d" % (int(obs[u'XPos']), int(obs[u'ZPos']))
-        self.logger.debug("State: %s (x = %.2f, z = %.2f)" % (current_s, float(obs[u'XPos']), float(obs[u'ZPos'])))
+        current_loc = "%d:%d" % (int(obs[u'XPos']), int(obs[u'ZPos']))
+        self.logger.debug("State: %s %d %d %d %d" % (current_s[0], current_s[1], current_s[2], current_s[3], current_s[4]))
         if not self.q_table.has_key(current_s):
             self.q_table[current_s] = ([0] * len(self.actions))
         # update Q values
@@ -213,37 +233,25 @@ class TabQAgent:
             self.canvas.grid()
             self.root.update()
         self.canvas.delete("all")
-        action_inset = 0.1
-        action_radius = 0.1
-        curr_radius = 0.2
-        action_positions = [ ( 0.5, action_inset ), ( 0.5, 1-action_inset ), ( action_inset, 0.5 ), ( 1-action_inset, 0.5 ) ]
         # (NSWE to match action order)
         min_value = -20
         max_value = 20
         for x in range(world_x):
             for y in range(world_y):
                 s = "%d:%d" % (x + self.min_x, y + self.min_z)
-                self.canvas.create_rectangle( x*scale, y*scale, (x+1)*scale, (y+1)*scale, outline="#fff", fill="#000")
-                for action in range(4):
-                    if not s in self.q_table:
-                        continue
-                    value = self.q_table[s][action]
-                    color = 255 * ( value - min_value ) / ( max_value - min_value ) # map value to 0-255
-                    color = max( min( color, 255 ), 0 ) # ensure within [0,255]
-                    color_string = '#%02x%02x%02x' % (255-color, color, 0)
-                    self.canvas.create_oval( (x + action_positions[action][0] - action_radius ) *scale,
-                                             (y + action_positions[action][1] - action_radius ) *scale,
-                                             (x + action_positions[action][0] + action_radius ) *scale,
-                                             (y + action_positions[action][1] + action_radius ) *scale,
-                                             outline=color_string, fill=color_string )
+                value = max( self.q_table[s][:] )
+                color = 255 * ( value - min_value ) / ( max_value - min_value ) # map value to 0-255
+                color = max( min( color, 255 ), 0 ) # ensure within [0,255]
+                color_string = '#%02x%02x%02x' % (255-color, color, 0)
+                if not s in self.q_table:
+                    self.canvas.create_rectangle( x*scale, y*scale, (x+1)*scale, (y+1)*scale, outline="#fff", fill="#000")
+                    continue
+                else:
+                    self.canvas.create_rectangle( x*scale, y*scale, (x+1)*scale, (y+1)*scale, outline=color_string, fill=color_string)
         if curr_x is not None and curr_y is not None:
             curr_x = curr_x - self.min_x
             curr_y = curr_y - self.min_z
-            self.canvas.create_oval( (curr_x + 0.5 - curr_radius ) * scale,
-                                     (curr_y + 0.5 - curr_radius ) * scale,
-                                     (curr_x + 0.5 + curr_radius ) * scale,
-                                     (curr_y + 0.5 + curr_radius ) * scale,
-                                     outline="#fff", fill="#fff" )
+            self.canvas.create_rectangle( curr_x*scale, curr_y*scale, (curr_x+1)*scale, (curr_y+1)*scale, outline="#fff", fill="#111")
         self.root.update()
 
 # TODO store reward_list, num_moves_per_episode, avg_q_value_per_episode
